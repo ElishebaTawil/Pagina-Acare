2705999776b62b946c39c2efa4681a42d66464c2
Nahuel <perez.dangelo.nahuel@gmail.com>
2023-11-06 02:53:45 -0300

    Minicart actualizado

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-
+document.addEventListener("DOMContentLoaded", function () {
 
 function validateForm() {
   // Validar nombre (solo letras y números)
@@ -81,153 +81,103 @@ const countProducts = document.querySelector('.ms-1');
 const cartEmpty = document.querySelector('.cart-empty');
 const cartTotal = document.querySelector('.cart-total');
 
-// Obtener los productos del almacenamiento local al cargar la página
-document.addEventListener('DOMContentLoaded', () => {
-const productsInStorage = localStorage.getItem('cartProducts');
-
-if (productsInStorage) {
-  allProducts = JSON.parse(productsInStorage);
-  showHTML();
-}
-});
 
 productsList.addEventListener('click', e => {
-if (e.target.classList.contains('mt-auto')) {
-  const product = e.target.parentElement;
+	if (e.target.classList.contains('mt-auto')) {
+		const product = e.target.parentElement;
 
-  const infoProduct = {
-    quantity: 1,
-    title: product.querySelector('.fw-bolder').textContent,
-    price: product.querySelector('.precio').textContent,
-  };
+		const infoProduct = {
+			quantity: 1,
+			title: product.querySelector('h5').textContent,
+			price: product.querySelector('p').textContent,
+		};
 
-  const exits = allProducts.some(product => product.title === infoProduct.title);
+		const exits = allProducts.some(
+			product => product.title === infoProduct.title
+		);
 
-  if (exits) {
-    const products = allProducts.map(product => {
-      if (product.title === infoProduct.title) {
-        product.quantity++;
-        return product;
-      } else {
-        return product;
-      }
-    });
-    allProducts = [...products];
-  } else {
-    allProducts = [...allProducts, infoProduct];
-  }
+		if (exits) {
+			const products = allProducts.map(product => {
+				if (product.title === infoProduct.title) {
+					product.quantity++;
+					return product;
+				} else {
+					return product;
+				}
+			});
+			allProducts = [...products];
+		} else {
+			allProducts = [...allProducts, infoProduct];
+		}
 
-  // Guardar los productos en el almacenamiento local
-  localStorage.setItem('cartProducts', JSON.stringify(allProducts));
-
-  showHTML();
-}
+		showHTML();
+	}
 });
-
 rowProduct.addEventListener('click', e => {
-if (e.target.classList.contains('icon-close')) {
-  const product = e.target.parentElement;
-  const title = product.querySelector('p').textContent;
+	if (e.target.classList.contains('icon-close')) {
+		const product = e.target.parentElement;
+		const title = product.querySelector('p').textContent;
 
-  allProducts = allProducts.filter(product => product.title !== title);
+		allProducts = allProducts.filter(
+			product => product.title !== title
+		);
 
-  // Guardar los productos en el almacenamiento local
-  localStorage.setItem('cartProducts', JSON.stringify(allProducts));
+		console.log(allProducts);
 
-  showHTML();
-}
+		showHTML();
+	}
 });
-
-// Función para mostrar el HTML del carrito
 const showHTML = () => {
-if (!allProducts.length) {
-  cartEmpty.classList.remove('hidden');
-  rowProduct.classList.add('hidden');
-  cartTotal.classList.add('hidden');
-} else {
-  cartEmpty.classList.add('hidden');
-  rowProduct.classList.remove('hidden');
-  cartTotal.classList.remove('hidden');
-}
+	if (!allProducts.length) {
+		cartEmpty.classList.remove('hidden');
+		rowProduct.classList.add('hidden');
+		cartTotal.classList.add('hidden');
+	} else {
+		cartEmpty.classList.add('hidden');
+		rowProduct.classList.remove('hidden');
+		cartTotal.classList.remove('hidden');
+	}
 
-// Limpiar HTML
-rowProduct.innerHTML = '';
+	// Limpiar HTML
+	rowProduct.innerHTML = '';
 
-let total = 0;
-let totalOfProducts = 0;
+	let total = 0;
+	let totalOfProducts = 0;
 
-allProducts.forEach(product => {
-  const containerProduct = document.createElement('div');
-  containerProduct.classList.add('cart-product');
+	allProducts.forEach(product => {
+		const containerProduct = document.createElement('div');
+		containerProduct.classList.add('cart-product');
 
-  containerProduct.innerHTML = `
-    <div class="info-cart-product">
-      <span class="cantidad-producto-carrito">${product.quantity}</span>
-      <p class="titulo-producto-carrito">${product.title}</p>
-      <span class="precio-producto-carrito">${product.price}</span>
-    </div>
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke-width="1.5"
-      stroke="currentColor"
-      class="icon-close"
-    >
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        d="M6 18L18 6M6 6l12 12"
-      />
-    </svg>
-  `;
+		containerProduct.innerHTML = `
+            <div class="info-cart-product">
+                <span class="cantidad-producto-carrito">${product.quantity}</span>
+                <p class="titulo-producto-carrito">${product.title}</p>
+                <span class="precio-producto-carrito">${product.price}</span>
+            </div>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="icon-close"
+            >
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                />
+            </svg>
+        `;
 
-  rowProduct.append(containerProduct);
+		rowProduct.append(containerProduct);
 
-  total += parseInt(product.quantity * product.price.slice(1));
-  totalOfProducts += product.quantity;
-});
+		total =
+			total + parseInt(product.quantity * product.price.slice(1));
+		totalOfProducts = totalOfProducts + product.quantity;
+	});
 
-valorTotal.innerText = `$${total}`;
-countProducts.innerText = totalOfProducts;
-
-// Verificar si se eliminaron todos los productos
-if (allProducts.length === 0) {
-  cartEmpty.innerText = 'El carrito está vacío';
-} else {
-  cartEmpty.innerText = ''; // Vaciar el texto del mensaje de carrito vacío
-}
+	valorTotal.innerText = `$${total}`;
+	countProducts.innerText = totalOfProducts;
 };
-
-
-
-
-
-/*--------------carrito de compras--------------*/
-
-/*-------------------Buscador-------------------*/
-
-
-document.getElementById('search-form').addEventListener('submit', function(event) {
-event.preventDefault(); // Evita que el formulario se envíe
-
-var searchTerm = document.getElementById('search-input').value.toLowerCase();
-
-// Redirige a la página correspondiente según el término de búsqueda
-if (searchTerm === 'accesorios') {
-  window.location.href = 'accesorios.html';
-} else if (searchTerm === 'remeras') {
-  window.location.href = 'remeras.html';
-} else if (searchTerm === 'buzos') {
-  window.location.href = 'buzos.html';
-} else if (searchTerm === 'pantalones') {
-  window.location.href = 'pantalones.html';
-} else {
-  // Si el término de búsqueda no coincide con ninguna opción, puedes mostrar un mensaje de error o realizar otra acción
-  alert('No se encontraron resultados para la búsqueda');
-}
 });
-
-/*-------------------Buscador-------------------*/
-
-
